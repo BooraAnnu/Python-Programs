26bb55edd0cb2f95f5e471ee610ecc4928368799
BooraAnnu <anshuboora164@gmail.com>
2021-03-16 18:34:14 +0530

    A program that print whether a number is even or odd

---
+++ even_odd.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 16 14:20:32 2021
+
+@author: hp
+"""
+
+
+num=int(input("Enter a number : "))
+if num % 2==0:
+    print(num,"is even")
+else:
+    print(num,"is odd")
