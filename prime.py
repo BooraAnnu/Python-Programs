83ce1a5d93a0f09c2be92acf6bc404f66e62250b
BooraAnnu <anshuboora164@gmail.com>
2021-03-16 18:17:22 +0530

    prime number program

---
+++ prime.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 16 13:28:24 2021
+
+@author: hp
+"""
+
+
+num= int(input("Enter a numer : "))
+if num >1:
+    for i in range(2,num):
+        if num % i ==0:
+            print(num,"is not prime")
+            break
+        else:
+            print(num,"is prime")
+            break
+else:
+    print(num,"is not prime")
