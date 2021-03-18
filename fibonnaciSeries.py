54ec9d2b8707418f2525ea92579ee59b6006b188
BooraAnnu <anshuboora164@gmail.com>
2021-03-18 10:19:53 +0530

    A program to print the series of fibonnaci numbers upto a particular user input

---
+++ fibonnaciSeries.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 18 08:50:29 2021
+
+@author: hp
+"""
+
+
+n= int(input("Enter how many fibonnaci numbers : "))
+f1=0
+f2=1
+if n<0:
+    print("Incorrect input")
+elif n==0:
+    print(f1)
+elif n==1:
+    print(f2)
+else:
+    print(f1,"\n", f2, sep="")
+    for i in range(2,n):
+        f = f1 + f2
+        print(f)
+        f1=f2
+        f2=f
