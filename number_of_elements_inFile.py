4b14f4f8e1c3afd67cb2379e3a0069f6b2a19529
BooraAnnu <anshuboora164@gmail.com>
2021-03-20 16:37:09 +0530

    get the numbers of elements in file

--- number_of_elements_inFile.py
+++ number_of_elements_inFile.py
@@ -5,9 +5,11 @@ Created on Thu Mar 18 09:39:25 2021
 @author: hp
 """
 
+# A  program to find the number of elements in a file
+
 count=0
 #open file in read mode
-file= open("C:\\Users\\hp\\Desktop\\python1\\data.txt", "r")
+file= open("C:\\Users\\hp\\Desktop\\Python-Programs\\data.txt", "r")
 
 for line in file:
     #Split each line into names
